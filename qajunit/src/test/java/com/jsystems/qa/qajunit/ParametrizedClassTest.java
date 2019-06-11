4b753d261849be3ad8e7781a7c8c65c4c274d730
*kberbec* <*kamil.berbec@gmail.com*>
2019-06-11 09:53:03 +0200

    First test

--- qajunit/src/test/java/com/jsystems/qa/qajunit/ParametrizedClassTest.java
+++ qajunit/src/test/java/com/jsystems/qa/qajunit/ParametrizedClassTest.java
@@ -1,6 +1,9 @@
 package com.jsystems.qa.qajunit;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -15,8 +18,23 @@ public class ParametrizedClassTest {
 
         assertTrue(value.contains("Hello"));
     }
+
+    @DisplayName("CSV second parametrized test")
+    @ParameterizedTest(name = "Parametrized test with value name {0} and value {1}")
+    @CsvSource(value = {"Hello, 5", "Hello Unit 5, 15", "'Hello 5!', 25"}, delimiter = ',')
+    public void paramMultiargTest(String param1, int param2) {
+
+        assertTrue(param1.contains("Hello"));
+        assertTrue(param2 % 5 == 0);
+    }
+
+
+    @DisplayName("CSV second parametrized test")
+    @ParameterizedTest(name = "Parametrized test with value name {0} and value {1}")
+    @CsvFileSource(resources = "/plik.csv", delimiter = ',')
+    public void paramCsvMultiargTest(String param1, int param2) {
+        assertTrue(param1.contains("Hello"));
+        assertTrue(param2 % 5 == 0);
+    }
 }
-
-
-
 
